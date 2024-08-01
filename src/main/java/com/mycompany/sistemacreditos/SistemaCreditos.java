e4ce544f5466fbb6371192ed399b036aab3796b3
altaparro <matiasaltaparro@hotmail.com>
2024-08-01 14:16:51 -0300

    conexiones entre formularios

---
+++ src/main/java/com/mycompany/sistemacreditos/SistemaCreditos.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemacreditos;
+
+public class SistemaCreditos {
+
+    public static void main(String[] args) {
+        new FormPrincipal().setVisible(true);
+    }
+}
